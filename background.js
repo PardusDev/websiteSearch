6ac9a779f12e864393d2c7835f6640e6f37ae2ba
PardusDev <rustemavci@outlook.com.tr>
2023-06-21 23:16:43 +0300

    Changes.

--- background.js
+++ background.js
@@ -1,24 +1,28 @@
+var Items = [
+    {
+        "id": "search-with-epey",
+        "title" : "Epey'de Ara",
+        "type" : "normal",
+        "contexts" : ["selection"],
+    },
+    {
+        "id": "search-with-bokyeme",
+        "title" : "BokYeme'de Ara",
+        "type" : "normal",
+        "contexts" : ["selection"],
+    }
+];
+
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
 	var url = "https://google.com.tr";
 	if (info.menuItemId === "search-with-epey") {
-		var url = "https://google.com.tr/search?q=" + info.selectionText + "%20site:epey.com&{google:RLZ}{google:originalQueryForSuggestion}{google:assistedQueryStats}{google:searchFieldtrialParameter}{google:iOSSearchLanguage}{google:prefetchSource}{google:searchClient}{google:sourceId}{google:contextualSearchVersion}ie={inputEncoding}"
+		url = "https://google.com.tr/search?q=" + info.selectionText + "%20site:epey.com&{google:RLZ}{google:originalQueryForSuggestion}{google:assistedQueryStats}{google:searchFieldtrialParameter}{google:iOSSearchLanguage}{google:prefetchSource}{google:searchClient}{google:sourceId}{google:contextualSearchVersion}ie={inputEncoding}"
 	} else if (info.menuItemId === "search-with-bokyeme") {
-		var url = "https://google.com.tr/search?q=" + info.selectionText + "%20site:bokyeme.com&{google:RLZ}{google:originalQueryForSuggestion}{google:assistedQueryStats}{google:searchFieldtrialParameter}{google:iOSSearchLanguage}{google:prefetchSource}{google:searchClient}{google:sourceId}{google:contextualSearchVersion}ie={inputEncoding}"	
+		url = "https://google.com.tr/search?q=" + info.selectionText + "%20site:bokyeme.com&{google:RLZ}{google:originalQueryForSuggestion}{google:assistedQueryStats}{google:searchFieldtrialParameter}{google:iOSSearchLanguage}{google:prefetchSource}{google:searchClient}{google:sourceId}{google:contextualSearchVersion}ie={inputEncoding}"
 	};
 	chrome.tabs.create({ url: url, });
 });
 
-
-chrome.contextMenus.create({
-  "id": "search-with-epey",
-  "title" : "Epey'de Ara",
-  "type" : "normal",
-  "contexts" : ["selection"],
-});
-
-chrome.contextMenus.create({
-  "id": "search-with-bokyeme",
-  "title" : "BokYeme'de Ara",
-  "type" : "normal",
-  "contexts" : ["selection"],
+Items.forEach(function(Item) {
+    chrome.contextMenus.create(Item);
 });
