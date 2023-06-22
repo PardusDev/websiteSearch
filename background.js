cdbe3c8b5ab2ebde77cb272f0c7f8b27fb0519b3
PardusDev <rustemavci@outlook.com.tr>
2023-06-23 00:42:38 +0300

    It has been made more user-friendly.

--- background.js
+++ background.js
@@ -1,9 +1,18 @@
+let Items;
+
 chrome.runtime.onInstalled.addListener(function(info, tab) {
 	fetch('https://pardusdev.github.io/extension/contextItems.json')
 		.then(response => response.json())
-		.then(Items  => {
+		.then(_Items  => {
+			Items = _Items;
 			Items.forEach(function(Item) {
-				chrome.contextMenus.create(Item);
+				var tempItem = {
+					id: Item.id,
+					title: Item.title,
+					type: Item.type,
+					contexts: Item.contexts
+				}
+				chrome.contextMenus.create(tempItem);
 			});
 		})
 		.catch(error => {
@@ -12,13 +21,12 @@ chrome.runtime.onInstalled.addListener(function(info, tab) {
 })
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-	var url = "https://google.com.tr";
-	if (info.menuItemId === "search-with-epey") {
-		url = "https://google.com.tr/search?q=" + info.selectionText + "%20site:epey.com&{google:RLZ}{google:originalQueryForSuggestion}{google:assistedQueryStats}{google:searchFieldtrialParameter}{google:iOSSearchLanguage}{google:prefetchSource}{google:searchClient}{google:sourceId}{google:contextualSearchVersion}ie={inputEncoding}"
-	} else if (info.menuItemId === "search-with-bokyeme") {
-		url = "https://google.com.tr/search?q=" + info.selectionText + "%20site:bokyeme.com&{google:RLZ}{google:originalQueryForSuggestion}{google:assistedQueryStats}{google:searchFieldtrialParameter}{google:iOSSearchLanguage}{google:prefetchSource}{google:searchClient}{google:sourceId}{google:contextualSearchVersion}ie={inputEncoding}"
-	};
+	var url = "https://google.com.tr/search?q=" + info.selectionText + "%20site:"+ getWebsiteAddressById(info.menuItemId);
 	chrome.tabs.create({ url: url, });
 });
 
+function getWebsiteAddressById(id) {
+	let Item = Items.find(item => item.id === id);
+	return Item.websiteAddress;
+}
 
